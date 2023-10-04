0425513bbfaeb3340c467e6bbc71cd7f28847eee
Dan <meredogukan@gmail.com>
2023-10-03 22:03:44 -0400

    security first part done

--- src/main/java/com/ticketing/config/SecurityConfig.java
+++ src/main/java/com/ticketing/config/SecurityConfig.java
@@ -4,43 +4,33 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder){
-
-        List<UserDetails> userList = new ArrayList<>();
-
-        userList.add(
-                new User("dgkn", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
-        userList.add(
-                new User("admin", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-
-
-        return new InMemoryUserDetailsManager(userList);
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder){
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//
+//        userList.add(
+//                new User("dgkn", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN"))));
+//        userList.add(
+//                new User("admin", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//
+//
+//        return new InMemoryUserDetailsManager(userList);
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authz) -> authz
-                        .requestMatchers("/user/**").hasRole("ADMIN")
-                        .requestMatchers("/project/**").hasRole("MANAGER")
-                        .requestMatchers("/task/employee/**").hasRole("EMPLOYEE")
-                        .requestMatchers("/task/**").hasRole("MANAGER")
+                        .requestMatchers("/user/**").hasAuthority("Admin")
+                        .requestMatchers("/project/**").hasAuthority("Manager")
+                        .requestMatchers("/task/employee/**").hasAuthority("Employee")
+                        .requestMatchers("/task/**").hasAuthority("Manager")
 //                        .requestMatchers("/task/**").hasAnyRole("EMPLOYEE", "ADMIN")
 //                        .requestMatchers("task/**").hasAuthority("ROLE_EMPLOYEE")
                         .requestMatchers("/login", "/", "fragments/**", "/assets/**", "/images/**").permitAll()
